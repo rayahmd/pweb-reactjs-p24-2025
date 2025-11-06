366cd7c77db19c6943130702ef2a7e04edc1a826
rayahmd <rayasyarif@gmail.com>
2025-11-07 06:24:08 +0700

    first commit fe

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+			<p className="text-white text-5xl font-bold text-center py-96">hay</p>
+    </>
+  )
+}
+
+export default App
